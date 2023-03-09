4e37b4a05eaa31a5dbe4770f1aaa37955cad9c28
papoyoalan <124863211+papoyoalan@users.noreply.github.com>
2023-03-09 16:39:43 +0700

    Update index.js
    perubahan pesan

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                        messaging_product:"whatsapp",
                        to:from,
                        text:{
-                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                           body:"Salam silaturahim dari kami Yayasan Surya Alam Indonesia. Mohon maaf akun WhatsApp ini tidak bisa digunakan untuk percakapan dua arah. Silahkan hubungi Admin kami untuk memulai percakapan dengan cara klik link berikut https://sociabuzz.com/ysai_official"
                        }
                    },
                    headers:{
